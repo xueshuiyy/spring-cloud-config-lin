69bf8359d082bf354ee124c64ac298054c596536
yanglin <xueshuiyy@126.com>
2019-05-05 15:14:57 +0800

    提交client工程

---
+++ config-client/src/main/java/lin/springcloud/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package lin.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+    @Value("${myconfigtest}") // git配置文件里的key
+    public String myconfigtest;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return myconfigtest;
+    }
+
+}
